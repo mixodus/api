38c2d57b7a17f15e47c65aeba9db000d5dc53928
Ubuntu <ubuntu@ip-172-31-24-19.ap-southeast-1.compute.internal>
2021-03-29 04:22:36 +0000

    updates

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -122,7 +122,7 @@ class EventController extends Controller
 	}
 	// ================================================
 	public function HackathonSemester(){
-		$data['format1'] = array(1,2,3,4,5,6,7,8);
+		$data['format1'] = array(6,7,8,9,10);
 		// for ($i=1; $i <= 8; $i++) { 
 		// 	$semester['semester'] = $i;
 		// 	$array[] = $semester;
