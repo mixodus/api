4d4c5248647addf6548dd9174ea73571e185ab95
Ivan Yunus <mixsodus@gmail.com>
2021-04-07 22:42:52 +0700

    fix

--- app/Http/Controllers/API/Dashboard/ReferralController.php
+++ app/Http/Controllers/API/Dashboard/ReferralController.php
@@ -56,7 +56,7 @@ class ReferralController extends Controller
 	}
 	public function getReferralMember(Request $request){
 		$getUser = $this->getDataServices->getAdminbyToken($request);
-		if($this->getDataServices->getProperty($getUser, 'role_id')==false){
+		if($this->getDataServices->getProperty($getUser, 'role_id')==3){
 			$getData = ReferralModel::select('*')->where('source','web')->with('AdminModel');
 		
 			if($request->referral_employee_id != null && $request->referral_employee_id !=""){
