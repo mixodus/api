c46e479dbe7220bdca6bca2493bd45454fa1b19f
Ivan Yunus <mixsodus@gmail.com>
2021-05-06 11:59:09 +0700

    votes update

--- app/Http/Controllers/Services/ActionServices.php
+++ app/Http/Controllers/Services/ActionServices.php
@@ -474,6 +474,9 @@ class ActionServices extends BaseController
 			return $getCandidate;
 		}
 		$temp = VoteChoiceSubmitModel::select('*')->where('employee_id', $user->user_id)->first();
+		if(empty($temp)){
+			return $temp;
+		}
 		if($temp['employee_id'] == $user->user_id){
 			return "false";
 		}
